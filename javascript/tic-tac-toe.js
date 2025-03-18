437296e252991a79f2f1d2619437cbf31f02e0e1
Andy Ford <andyford1993@gmail.com>
2025-03-18 14:39:47 -0600

    Tic tac toe functional!!!

--- javascript/tic-tac-toe.js
+++ javascript/tic-tac-toe.js
@@ -1,6 +1,6 @@
 const cells = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#statusText");
-const restartButton = document.querySelectorAll("restartButton");
+const restartButton = document.querySelector("#restartButton");
 const winConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -19,7 +19,7 @@ initializeGame();
 
 function initializeGame() {
     cells.forEach(cell => cell.addEventListener("click", cellClicked));
-    // restartButton.addEventListener("click", restartGame);
+    restartButton.addEventListener("click", restartGame);
     statusText.textContent = `${currentPlayer}'s turn`;
     running = true;
 
@@ -45,8 +45,34 @@ function changePlayer() {
 
 }
 function checkWinner() {
+    let roundWon = false;
 
+    for(let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == "") {
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC) {
+            roundWon = true;
+            break;
+        }
+    }
+    if (roundWon) {
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false; 
+    } else if (!options.includes("")) {
+        statusText.textContent = "Draw!";
+        running = false;
+    } else {
+        changePlayer();
+    }
 }
 function restartGame() {
-
+    initializeGame();
+    options = ["", "", "", "", "", "", "", "", ""];
+    cells.forEach(cell => cell.textContent = "");
 }
